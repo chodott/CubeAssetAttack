3037069f567d964ad6d4c4539cd8c1a6592c9519
chodot <89974193+chodott@users.noreply.github.com>
2025-01-16 19:06:49 +0900

    적 위치를 기준으로 타워 Pitch축 회전으로 에임 조정

--- Assets/Original/Friend/Script/Friend.cs
+++ Assets/Original/Friend/Script/Friend.cs
@@ -5,10 +5,12 @@ public class Friend : MonoBehaviour
     [SerializeField]
     private Weapon _equippedWeapon;
     private Transform _targetTransform;
+    private Animator _animator;
     private float _attackRange = 5.0f;
     private float _rotateSpeed = 10.0f;
-    void Start()
+    protected void Start()
     {
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -44,10 +46,18 @@ public class Friend : MonoBehaviour
 
     private void TurnToTarget(float deltaTime)
     {
-        Vector3 directionVector = _targetTransform.position - transform.position;
-        directionVector.y = 0;
-        Quaternion curRotation =  Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(directionVector), deltaTime * _rotateSpeed);
+        Vector3 directionVector = _targetTransform.position  - transform.position;
+        Vector3 directionXZ = directionVector;
+        directionXZ.y = 0;
+        Quaternion curRotation =  Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(directionXZ), deltaTime * _rotateSpeed);
         transform.rotation = curRotation;
+
+
+        float angle = Vector3.Angle(directionXZ, directionVector);
+        _animator.SetFloat("Angle", angle/ 45.0f);
+        _animator.SetFloat("Blend", 0.1f);
+        Debug.Log(angle);
+
     }
 
     private bool CheckTargetInRange()
